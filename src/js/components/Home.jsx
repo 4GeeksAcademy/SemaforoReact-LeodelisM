8ff94d3b8011249cff796615b13c5a353dcbc44f
Leodelis <leodelis19@gmail.com>
2025-02-11 17:42:19 +0000

    SemaforoConBoton1

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Semaforo from './Semaforo';
 
+
 export default function Home() {
     return (
         <>
--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
 
 export default function Semaforo() {
-    const [luzActiva, setLuzActiva] = useState("roja");
+    const [luzActiva, setLuzActiva] = useState("null");
+
+    // Función para cambiar colores, asociada al botón "Cambiar Color"
+    const cambiarColores = () => {
+        if (luzActiva === "null" || luzActiva === "verde") {
+            setLuzActiva("roja");
+        } else if (luzActiva === "roja") {
+            setLuzActiva("amarillo");
+        } else if (luzActiva === "amarillo") {
+            setLuzActiva("verde");
+        }
+    };
 
     return (
+        <div>
+            <div className="contenedor-base">
+                <div
+                    className={`luz-roja ${luzActiva === "roja" ? "luz-activa" : ""}`}
+                    onClick={() => setLuzActiva("roja")}
+                ></div>
+                <div
+                    className={`luz-amarillo ${luzActiva === "amarillo" ? "luz-activa" : ""}`}
+                    onClick={() => setLuzActiva("amarillo")}
+                ></div>
+                <div
+                    className={`luz-verde ${luzActiva === "verde" ? "luz-activa" : ""}`}
+                    onClick={() => setLuzActiva("verde")}
+                ></div>
+            </div>
 
-        // <button onClick={aquimetoalgo}></button>
-
-        <div className="contenedor-base">
-            <div className={`luz-roja ${luzActiva === "roja" ? "luz-activa" : ""}`} onClick={
-                () => {
-                    setLuzActiva("roja");
-                }
-            }></div>
-            <div className={`luz-amarillo ${luzActiva === "amarillo" ? "luz-activa" : ""}`} onClick={
-                () => {
-                    setLuzActiva("amarillo")
-                }
-            }></div>
-            <div className={`luz-verde ${luzActiva === "verde" ? "luz-activa" : ""}`} onClick={
-                () => {
-                    setLuzActiva("verde")
-                }
-            }></div>
+            <div className="boton-cambiar-color">
+                <button
+                    onClick={cambiarColores}
+                    className="cambiar-color"
+                >
+                    Cambiar Color
+                </button>
+            </div>
         </div>
     );
 }
